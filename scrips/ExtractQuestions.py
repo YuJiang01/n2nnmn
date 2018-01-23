3cc1196b64b6c7f0b0d3b098c3ed6ca6e9e145d9
Yu Jiang <tinayujiang@fb.com>
2018-01-23 14:55:02 -0800

    modify extract to python3

--- scrips/ExtractQuestions.py
+++ scrips/ExtractQuestions.py
@@ -4,10 +4,11 @@ import sys
 datafile = sys.argv[1]
 data = json.load(open(datafile))
 
-print 'question_index\t ques\tfunction\tanswer\timage_index\tquestion_family_index'
+print ('question_index\t ques\tfunction\tanswer\timage_index\tquestion_family_index')
+
 for ques in data['questions']:
-    functions = ','.join(list(map(lambda x: x['function'],ques['program'])));
-    print '{question_index}\t"{ques}"\t{function}\t{answer}\t{image_index}\t{question_family_index}'.\
+    functions = ','.join(list(map(lambda x: x['function'],ques['program'])))
+    print ('{question_index}\t"{ques}"\t{function}\t{answer}\t{image_index}\t{question_family_index}'.\
         format(ques=ques['question'], question_index=ques['question_index'],function=functions,answer=ques['answer'],
                image_index=ques['image_index'],question_family_index=ques['question_family_index']
-               )
+               ))
