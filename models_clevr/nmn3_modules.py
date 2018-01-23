c0a7cdff3f025bc71c0042106f1f66838947f799
Tina Jiang <tinayujiang@devfair0161.h2.fair>
2018-01-23 12:13:16 -0700

    preliminary change

--- models_clevr/nmn3_modules.py
+++ models_clevr/nmn3_modules.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import convert_to_tensor as to_T
 
-from util.cnn import fc_layer as fc, conv_layer as conv
+from util.cnn import fc_layer as fc, conv_layer as conv,fc_relu_layer as fc_relu
 from util.empty_safe_conv import empty_safe_1x1_conv as _1x1_conv
 from util.empty_safe_conv import empty_safe_conv as _conv
 
@@ -280,7 +280,7 @@ class Modules:
         return scores
 
     def CountModule(self, input_0, time_idx, batch_idx,
-        scope='CountModule', reuse=True):
+        scope='CountModule',kernel_size=5, map_dim=300, reuse=True):
         # In TF Fold, batch_idx and time_idx are both [N_batch, 1] tensors
 
         # Mapping: att_grid -> answer probs
@@ -293,13 +293,20 @@ class Modules:
         #   1. linear transform of the attention map (also including max and min)
         with tf.variable_scope(self.module_variable_scope):
             with tf.variable_scope(scope, reuse=reuse):
-                H, W = self.att_shape[1:3]
-                att_all = tf.reshape(input_0, to_T([-1, H*W]))
-                att_min = tf.reduce_min(input_0, axis=[1, 2])
-                att_max = tf.reduce_max(input_0, axis=[1, 2])
+                #H, W = self.att_shape[1:3]
+                #att_all = tf.reshape(input_0, to_T([-1, H*W]))
+                #att_min = tf.reduce_min(input_0, axis=[1, 2])
+                #att_max = tf.reduce_max(input_0, axis=[1, 2])
                 # att_reduced has shape [N, 3]
-                att_concat = tf.concat([att_all, att_min, att_max], axis=1)
-                scores = fc('fc_scores', att_concat, output_dim=self.num_choices)
+                #att_concat = tf.concat([att_all, att_min, att_max], axis=1)
+                #scores = fc('fc_scores', att_concat, output_dim=self.num_choices)
+		att_maps = _conv('conv_maps', input_0, kernel_size=kernel_size,stride=1, output_dim=map_dim)
+		att_shape = tf.shape(att_maps)
+                N = att_shape[0]
+                H = att_shape[1]
+                W = att_shape[2]
+		att_all = tf.reshape(att_maps, to_T([-1, H*W]))
+		scores = fc('fc_scores', att_all, output_dim=self.num_choices)
 
         return scores
 
